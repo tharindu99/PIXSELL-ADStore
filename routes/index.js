c5195ade6ff7faf79394508ad453eb7e680435bc
tharindu99 <madushanka.best@gmail.com>
2017-12-24 00:39:10 +0530

    project intialization 2

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+var TopBar = {
+  project_name:'PIXSELL AD Store',
+  searchShow:'none',
+  notificationShow:'none',
+  taskShow:'none',
+}
+var LeftSlideBar = {
+  user : 'none'
+}
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home',{title:'PIXSELL AD Store',TopBar:TopBar,LeftSlideBar:LeftSlideBar});
 });
 
 module.exports = router;
