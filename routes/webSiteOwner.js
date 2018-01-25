f1398ceb48f90c89058c71895934325d90b4634b
tharindu99 <madushanka.best@gmail.com>
2018-01-25 16:36:18 +0530

    menu updated

--- routes/webSiteOwner.js
+++ routes/webSiteOwner.js
@@ -8,19 +8,9 @@ var user = {
   image:'../../../../../images/user.png'
 }
 
-router.get('/', function(req, res, next) {  
-
-  var numberOfblocks = models.adblock.findAndCountAll({
-    where:{
-      ownerid: user.id,
-      status:'active'
-    }
-  }).then(his_adblocks=>{
-
-
-    
+router.get('/', function(req, res, next) {      
     var pageBasic = {
-      page_title:'PIXSELL'
+        page_title:'PIXSELL'
     }
     var headerBar = {
       header_title:'PIXSELL AD-Store - WEB SITE OWNER',
@@ -32,42 +22,123 @@ router.get('/', function(req, res, next) {
         image:user.image,
         subtitile:'ABC.lk | BC.com'
       },
-  
       menu:{
-          items :[
-            {name:'Home',class:'active',url:'/',icon:'home'},
-            {name:'AD Blocks',class:'normal',url:'webSiteOwner/adblocks',icon:'widgets'},
-            {name:'New AD Block',class:'normal',url:'webSiteOwner/newBlock',icon:'crop_square'},
-            {name:'My Account',class:'normal',url:'webSiteOwner/myaccount',icon:'verified_user'},
-            {name:'Help',class:'normal',url:'#',icon:'help'},
-          ]
+        items :[
+          {name:'Home',class:'active',url:'#',icon:'home'},
+          {name:'AD Blocks',class:'normal',url:'./adblocks',icon:'widgets'},
+          {name:'New AD Block',class:'normal',url:'./newBlock',icon:'crop_square'},
+          {name:'My Account',class:'normal',url:'./myaccount',icon:'verified_user'},
+          {name:'Help',class:'normal',url:'#',icon:'help'},
+        ]
       },
       footer:{
       }
     } 
     var page_content = {
-      dash4panels:{
-        items : [
-          {name:'Active Blocks',width:4,color:'red',icon:'widgets',value:his_adblocks.count},
-          {name:'Earnings',width:4,color:'green',icon:'monetization_on',value:'54K'},
-          {name:'Micro Investors',width:4,color:'blue',icon:'grain',value:2468}
-        ]
-      }
+      // dash4panels:{
+      //   items : [
+      //     {name:'Active Blocks',width:4,color:'red',icon:'widgets',value:his_adblocks.count},
+      //     {name:'Earnings',width:4,color:'green',icon:'monetization_on',value:'54K'},
+      //     {name:'Micro Investors',width:4,color:'blue',icon:'grain',value:2468}
+      //   ]
+      // }
     }
-var output = {pageBasic:pageBasic,headerBar:headerBar,leftMenu:leftMenu,page_content:page_content}
-res.render('webSiteOwner',output);
-  }); 
-
-          
-});
+    var output = {pageBasic:pageBasic,headerBar:headerBar,leftMenu:leftMenu,page_content:page_content}
+  res.render('webSiteOwner',output);
+}); 
 
 router.get('/adblocks',function(req,res,next){
-  res.render('webSiteOwner',adblocks_component.adblocks_component(demo_user));
+  var block_fillData=[];
+  var blocks =  models.adblock.findAll({
+        where: {
+          ownerid: user.id
+        }
+      }).then(blockInst => {
+          blockInst.forEach(blk => {
+              var temp_earn = randomGen.earn()[0];
+              var temp_complete = randomGen.complete()[0];
+              var temp_imp = randomGen.imp()[0];
+              var temp_demand = randomGen.demand()[0];
+  
+              block_fillData.push({
+                  name:blk.websitename+" : "+blk.name,
+                  subTitle:'since '+blk.start,
+                  size:6,
+                  color:randomGen.color(),
+                  widget_earn:{value:temp_earn.earn_Amount,data_arr:temp_earn.earn_Arr},
+                  widget_complete:{value:temp_complete.complete_Value,data_arr:temp_complete.complete_Arr},
+                  widget_imp:{value:temp_imp.imp_Value,data_arr:temp_imp.imp_Arr},
+                  widget_demand:{value:temp_demand.demanding_Value,data_arr:temp_demand.demand_Arr}
+              });
+          });
+          var pageBasic = {
+            page_title:'PIXSELL'
+          }
+          var headerBar = {
+            header_title:'PIXSELL AD-Store - WEB SITE OWNER',
+            header_title_URL:'./'
+          }
+          var leftMenu = {
+            user:{
+              name:user.name,
+              image:user.image,
+              subtitile:'ABC.lk | BC.com'
+            },
+            menu:{
+              items :[
+                {name:'Home',class:'active',url:'./',icon:'home'},
+                {name:'AD Blocks',class:'normal',url:'./adblocks',icon:'widgets'},
+                {name:'New AD Block',class:'normal',url:'./newBlock',icon:'crop_square'},
+                {name:'My Account',class:'normal',url:'./myaccount',icon:'verified_user'},
+                {name:'Help',class:'normal',url:'#',icon:'help'},
+              ]
+            },
+            footer:{
+            }
+          } 
+          var page_content = {
+                adblocksView:{ 
+                  blocks : block_fillData
+                }
+              }
+          var output = {pageBasic:pageBasic,headerBar:headerBar,leftMenu:leftMenu,page_content:page_content}
+    res.render('webSiteOwner',output);
+  });
 });
 
 router.get('/newBlock',function(req,res,next){
-  res.render('webSiteOwner',base_component.base_component(demo_user));
-});
+          var pageBasic = {
+            page_title:'PIXSELL'
+          }
+          var headerBar = {
+            header_title:'PIXSELL AD-Store - WEB SITE OWNER',
+            header_title_URL:'./'
+          }
+          var leftMenu = {
+            user:{
+              name:user.name,
+              image:user.image,
+              subtitile:'ABC.lk | BC.com'
+            },
+            menu:{
+              items :[
+                {name:'Home',class:'active',url:'./',icon:'home'},
+                {name:'AD Blocks',class:'normal',url:'./adblocks',icon:'widgets'},
+                {name:'New AD Block',class:'normal',url:'./newBlock',icon:'crop_square'},
+                {name:'My Account',class:'normal',url:'./myaccount',icon:'verified_user'},
+                {name:'Help',class:'normal',url:'#',icon:'help'},
+              ]
+            },
+            footer:{
+            }
+          } 
+          var page_content = {
+            block_register:{ 
+            }
+          }
+      var output = {pageBasic:pageBasic,headerBar:headerBar,leftMenu:leftMenu,page_content:page_content}
+    res.render('webSiteOwner',output);
+  });
 
 router.get('/myaccount',function(req,res,next){
   res.render('webSiteOwner',base_component.base_component(demo_user));
